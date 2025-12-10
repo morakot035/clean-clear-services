4f79dd3d3f6484125f43623238014100157e3083
morakot maneelart <dev.morakot@gmail.com>
2025-12-10 16:02:33 +0700

    muter

--- src/routes/bingo.routes.js
+++ src/routes/bingo.routes.js
@@ -1,4 +1,6 @@
 import express from "express";
+import multer from "multer";
+
 import {
   getProgress,
   updateTask,
@@ -8,6 +10,8 @@ import {
 import { upload } from "../middlewares/upload.middleware.js";
 import { verifyToken } from "../middlewares/verifyToken.js";
 
+const upload = multer({ dest: "uploads/" });
+
 const router = express.Router();
 
 router.get("/progress", verifyToken, getProgress);
@@ -15,7 +19,6 @@ router.post("/update", verifyToken, updateTask);
 router.get("/leaderboard", verifyToken, leaderBoard);
 router.post(
   "/upload",
-  verifyToken,
   upload.single("image"), // field name จาก frontend: formData.append("image", file)
   uploadImage
 );
